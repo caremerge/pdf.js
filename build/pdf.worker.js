cd78e861335d86167bdd1c6fe0c553db6754a92c
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2019-02-01 15:58:33 +0500

    remove print button completely; fix error

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -28,8 +28,8 @@ factory((root.pdfjsDistBuildPdfWorker = {}));
   // Use strict in our context only - users might not want it
   'use strict';
 
-var pdfjsVersion = '1.5.223';
-var pdfjsBuild = '3c55ecfb';
+var pdfjsVersion = '1.5.224';
+var pdfjsBuild = '4c8e99a0';
 
   var pdfjsFilePath =
     typeof document !== 'undefined' && document.currentScript ?
