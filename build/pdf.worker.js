12c7e2bcd006785b42deee17d725366eceaf526a
Umar Muneer <umarmuneer@Umars-MacBook-Pro.local>
2020-05-07 17:57:16 +0500

    ran npx gulp generic

--- build/pdf.worker.js
+++ build/pdf.worker.js
@@ -28,8 +28,8 @@ factory((root.pdfjsDistBuildPdfWorker = {}));
   // Use strict in our context only - users might not want it
   'use strict';
 
-var pdfjsVersion = '1.5.229';
-var pdfjsBuild = '6f3a5463';
+var pdfjsVersion = '1.5.231';
+var pdfjsBuild = '3b52f6f2';
 
   var pdfjsFilePath =
     typeof document !== 'undefined' && document.currentScript ?
