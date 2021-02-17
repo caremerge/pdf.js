97e3d6da34e90d16e07ada42984c148b7146f312
Nida Farooqui <nida.farooqui@careaxiom.com>
2021-02-17 11:47:25 +0500

    modified viewer, created a new viewer and updated the build.

--- build/generic/build/pdf.worker.js
+++ build/generic/build/pdf.worker.js
@@ -50,8 +50,8 @@ Object.defineProperty(exports, "WorkerMessageHandler", ({
 
 var _worker = __w_pdfjs_require__(1);
 
-const pdfjsVersion = '2.8.98';
-const pdfjsBuild = '04879bae5';
+const pdfjsVersion = '2.8.101';
+const pdfjsBuild = '3e0b1249a';
 
 /***/ }),
 /* 1 */
@@ -145,7 +145,7 @@ class WorkerMessageHandler {
     var WorkerTasks = [];
     const verbosity = (0, _util.getVerbosityLevel)();
     const apiVersion = docParams.apiVersion;
-    const workerVersion = '2.8.98';
+    const workerVersion = '2.8.101';
 
     if (apiVersion !== workerVersion) {
       throw new Error(`The API version "${apiVersion}" does not match ` + `the Worker version "${workerVersion}".`);
